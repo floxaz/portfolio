d8356793dcddea4ab5213b5da03430698295b487
Rostyslav Yatsura <floxaz1@gmail.com>
2019-03-20 22:56:28 +0100

    added contact dashboard

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,21 +1,29 @@
 import React from 'react';
 
 const Contact = () => (
-    <section className="contact">
-      <div className="row">
-         <svg className="contact__icon">
-           <use href="/images/sprite.svg#icon-mail4"></use>
-         </svg>
+  <section className="contact">
+    <div className="row">
+      <div className="contact__dashboard">
+        <a href="mailto:rossty4422@gmail.com" className="contact__link">
+          <svg className="contact__icon">
+            <use href="/images/sprite.svg#icon-mail4"></use>
+          </svg>
+        </a>
 
-         <svg className="contact__icon">
-           <use href="/images/sprite.svg#icon-github"></use>
-         </svg>
+        <a href="https://github.com/floxaz" target="_blank" className="contact__link">
+          <svg className="contact__icon">
+            <use href="/images/sprite.svg#icon-github"></use>
+          </svg>
+        </a>
 
-         <svg className="contact__icon">
-           <use href="/images/sprite.svg#icon-linkedin"></use>
-         </svg>
+        <a href="#" className="contact__link">
+          <svg className="contact__icon">
+            <use href="/images/sprite.svg#icon-linkedin"></use>
+          </svg>
+        </a>
       </div>
-    </section>
+    </div>
+  </section>
 );
 
 export default Contact;
