b96314e4e115ff916cf7f362b36e35e34b8bc2cb
Rostyslav Yatsura <floxaz1@gmail.com>
2019-03-21 23:56:07 +0100

    added links to eacg project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,17 +3,46 @@ import React from 'react';
 const Projects = () => (
     <section className="projects">
         <div className="row projects__display-flex">
-            <figure className="projects__item">
-                <img src="images/project-3.png" alt="view calculator" className="projects__img" />
-            </figure>
+            <div className="projects__item">
+                <figure>
+                    <img src="images/project-3.png" alt="view calculator" className="projects__img" />
+                </figure>
+                <div className="project">
+                    <div className="project__option">
+                        <a href="#" className="project__link">watch</a>
+                    </div>
+                    <div className="project__option">
+                        <a href="#" className="project__link">source code</a>
+                    </div>
+                </div>
+            </div>
+            <div className="projects__item">
+                <figure>
+                    <img src="images/project-2.png" alt="view calculator" className="projects__img" />
+                </figure>
+                <div className="project">
+                    <div className="project__option">
+                        <a href="#" className="project__link">watch</a>
+                    </div>
+                    <div className="project__option">
+                        <a href="#" className="project__link">source code</a>
+                    </div>
+                </div>
+            </div>
 
-            <figure className="projects__item">
-                <img src="images/project-2.png" alt="view calculator" className="projects__img" />
-            </figure>
-
-            <figure className="projects__item">
-                <img src="images/project-1.png" alt="view calculator" className="projects__img" />
-            </figure>
+            <div className="projects__item">
+                <figure>
+                    <img src="images/project-1.png" alt="view calculator" className="projects__img" />
+                </figure>
+                <div className="project">
+                    <div className="project__option">
+                        <a href="#" className="project__link">watch</a>
+                    </div>
+                    <div className="project__option">
+                        <a href="#" className="project__link">source code</a>
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
 );
