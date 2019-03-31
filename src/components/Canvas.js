2d6f6c5b3fb04967b089c24b375d0f9e6f16bfdb
Rostyslav Yatsura <floxaz1@gmail.com>
2019-03-31 12:53:50 +0200

    fixed bugs on canvas animation

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -5,107 +5,69 @@ class Canvas extends React.Component {
         super(props);
     }
 
-    state = {
-        randomCircles: []
-    };
-
     componentDidMount = () => {
-        this.setState(() => ({
-            randomCircles: this.getRandomCircles()
-        }));
-        console.log(this.refs.canvas.offsetHeight);
-        //console.log(this.refs.canvas.offsetWidth);
-    };
-    
-    componentDidUpdate = () => {
         const canvas = this.refs.canvas;
-        canvas.width = this.props.headerWidth;
-        canvas.height = this.props.headerHeight;
-        //console.log(canvas.width, canvas.height);
-
-        const ctx = this.refs.canvas.getContext('2d');
-        const animate = circles => {
-            requestAnimationFrame(() => {
-                animate(circles);
-            });
-            ctx.clearRect(0, 0, this.props.headerWidth, this.props.headerHeight);
-            
-            circles.forEach(circle => circle.update());
-        };
-
-        //const circles = this.getRandomCircles();
-        animate(this.getRandomCircles());
-    };
-
-
-    getRandomCircles = (num = 50) => {
-        const ctx = this.refs.canvas.getContext('2d');
-        //const headerWidth = this.props.headerWidth;
-        //const headerHeight = this.props.headerHeight;
-        const headerWidth = this.refs.canvas.offsetWidth;
-        const headerHeight = this.refs.canvas.offsetHeight;
+        const ctx = canvas.getContext('2d');
         const radius = 15;
+        const circlesQuantity = 50;
         let circles = [];
-        console.log(headerWidth, headerHeight);
+        canvas.width = canvas.offsetWidth;
+        canvas.height = canvas.offsetHeight;
 
-        function Circle(x, y, speedX, speedY, radius, ctx) {
+        function Circle(x, y, speedX, speedY, radius, ctx, width = canvas.width, height = canvas.height) {
             this.x = x;
             this.y = y;
             this.speedX = speedX;
             this.speedY = speedY;
             this.radius = radius;
             this.ctx = ctx;
-    
+            this.width = width;
+            this.height = height;
+
             this.draw = () => {
-                //const ctx = document.getElementById('canvas').getContext('2d');
                 ctx.beginPath();
                 ctx.arc(this.x, this.y, this.radius, Math.PI * 2, false);
                 ctx.strokeStyle = '#34495e';
-                ctx.stroke();
+                ctx.stroke();   
             };
-    
+
             this.update = () => {
-                if((this.x + this.radius) > headerWidth || (this.x - radius) < 0) {
+                if ((this.x + this.radius) > canvas.width || (this.x - this.radius) < 0) {
                     this.speedX = -this.speedX;
                 }
-    
-                if((this.y + radius) > headerHeight || (this.y - radius) < 0) {
+
+                if ((this.y + radius) > canvas.height || (this.y - this.radius) < 0) {
                     this.speedY = -this.speedY;
                 }
 
                 this.x += this.speedX;
                 this.y += this.speedY;
-    
-                this.draw();
-            }
-        };
-        
 
-        for (let i = 0; i < num + 1; i++) {
-            const x = Math.random() * (this.props.headerWidth - radius * 2) + radius;
-            const y = Math.random() * (this.props.headerHeight - radius * 2) + radius;
+                this.draw();
+            };
+        };
+
+        const animate = () => {
+            requestAnimationFrame(animate);
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            circles.forEach(circle => circle.update());
+        };
+
+        for (let i = 0; i < circlesQuantity; i++) {
+            const x = Math.random() * (canvas.width - radius * 2) + radius;
+            const y = Math.random() * (canvas.height - radius * 2) + radius;
             const speedX = Math.random() - 0.5;
             const speedY = Math.random() - 0.5;
-            circles.push(new Circle(x, y, speedX, speedY, radius, ctx));
+            circles.push(new Circle(x, y, speedX, speedY, radius, ctx, canvas.width, canvas.height));
         }
-        //console.log(circles);
-        return circles;
-    };
 
-    /*
-    drawCircles = (num = 50) => {
-        const ctx = this.refs.canvas.getContext('2d');
-        const radius = 15;
-
-        for (i = 0; i < num + 1; i++) {
-            ctx.beginPath();
-            const { x, y } = getRandomCoords();
-            ctx.arc(x, y, radius, Math.PI * 2, false);
-            ctx.strokeStyle = 'purple';
-            ctx.stroke();
-        }
+        animate();
+        
+        window.addEventListener('resize', () => {         
+           canvas.width = canvas.offsetWidth;
+           canvas.height = canvas.offsetHeight;
+        });       
     };
-    */
 
     render = () => {
         return (
@@ -115,7 +77,7 @@ class Canvas extends React.Component {
             >
             </canvas>
         );
-    }
+    };
 }
 
 export default Canvas;
--- src/components/Home.js
+++ src/components/Home.js
@@ -4,41 +4,19 @@ import Projects from './Projects';
 import Footer from './Footer';
 import Canvas from './Canvas';
 
-class Home extends React.Component {
-  state = {
-    headerWidth: undefined,
-    headerHeight: undefined
-  };
-
-  componentDidMount = () => {
-    this.getHeaderSize();
-    window.addEventListener('resize', this.getHeaderSize);
-  };
-
-  getHeaderSize = () => {
-    const header = this.refs.header;
-    this.setState(() => ({
-      headerWidth: header.offsetWidth,
-      headerHeight: header.offsetHeight
-    }));
-  };
-
-  render() {
-    return (
-      <React.Fragment>
-        <header ref="header" className="header">
-          <Canvas headerWidth={this.state.headerWidth} headerHeight={this.state.headerHeight} />
-          <div className="row">
-            <h1 className="header__headline">Ross Yatsura</h1>
-            <p>frontend developer based in Venice</p>
-          </div>
-        </header>
-        <Contact />
-        <Projects />
-        <Footer />
-      </React.Fragment>
-    )
-  };
-};
+const Home = () => (
+  <React.Fragment>
+    <header className="header">
+      <Canvas />
+      <div className="row">
+        <h1 className="header__headline">Ross Yatsura</h1>
+        <p>frontend developer based in Venice</p>
+      </div>
+    </header>
+    <Contact />
+    <Projects />
+    <Footer />
+  </React.Fragment>
+);
 
 export default Home;
