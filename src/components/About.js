cebfef7588480777efd3dd4b5c47a8a34df51b65
Rostyslav Yatsura <floxaz1@gmail.com>
2019-04-04 20:21:47 +0200

    added animations in about section

--- src/components/About.js
+++ src/components/About.js
@@ -3,28 +3,28 @@ import React from 'react';
 const About = props => (
   <div className="about">
     <div className="row about__display-flex">
-      <figure>
+      <figure className="about__portrait">
         <img src="images/portrait.jpg" className="about__img" alt="portrait"></img>
       </figure>
       <div className="about__description">
-        <h2>Hello,</h2>
-        <p>
+        <h2 className="about__headline">Hello,</h2>
+        <p className="about__paragraph about__paragraph--1">
           my full name is Rostylsav Yatsura and I am a passionate web developer,
           who specializes in creating dynamic and beautiful web pages.
           The creation process involves the use of the latest technologies of our
           fast-paced digital world.
          </p>
-        <p>
+        <p className="about__paragraph about__paragraph--2">
           My primary focus is frontend web development with React.js.
           I love dedicating days and even weeks to sharpen every pixel
           of my personal projects, before showing them to the light.
          </p>
-        <p>
+        <p className="about__paragraph about__paragraph--3">
           All in all, it would be a real pleasure for me if my passion and interest
           in tech could solve people's problems and make their life easier.
           Therefore, in case you have any questions, don't hesitate to contact me.
          </p>
-         <h2>Skills</h2>
+         <h2 className="about__headline">Skills</h2>
          <div className="about__skills">
            <div className="about__skill">html</div>
            <div className="about__skill">css</div>
