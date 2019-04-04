0fe4346965f39da6e8fa6ecad7f1a1d92d91510b
Rostyslav Yatsura <floxaz1@gmail.com>
2019-04-04 22:11:59 +0200

    styled 404 page

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const PageNotFound = () => (
-    <div>
-      <h1>404</h1>
-      <p>page not found</p>
+  <div className="notFound">
+    <div className="notFound__message">
+      <h1 className="notFound__headline">404</h1>
+      <p className="notFound__error">The page you requested couldn't be found</p>
+      <Link to="/" className="notFound__link">Back To Home</Link>
     </div>
+  </div>
 );
 
 export default PageNotFound;
