69c7f5f361b8bfd123c98b438f20023d36ea9fc5
Rostyslav Yatsura <floxaz1@gmail.com>
2019-04-04 18:37:24 +0200

    fixed hover after animation

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,37 +4,43 @@ const Projects = () => (
     <section className="projects">
         <div className="row projects__display-flex">
             <div className="projects__item projects__item--1">
-                <div className="projects__img projects__img--1"></div>
-                <div className="project">
-                    <div className="project__option">
-                        <a href="#" className="project__link">watch</a>
-                    </div>
-                    <div className="project__option">
-                        <a href="#" className="project__link">source code</a>
+                <div className="projects__content">
+                    <div className="projects__img projects__img--1"></div>
+                    <div className="project">
+                        <div className="project__option">
+                            <a href="#" className="project__link">watch</a>
+                        </div>
+                        <div className="project__option">
+                            <a href="#" className="project__link">source code</a>
+                        </div>
                     </div>
                 </div>
             </div>
 
             <div className="projects__item projects__item--2">
-                <div className="projects__img projects__img--2"></div>
-                <div className="project">
-                    <div className="project__option">
-                        <a href="#" className="project__link">watch</a>
-                    </div>
-                    <div className="project__option">
-                        <a href="#" className="project__link">source code</a>
+                <div className="projects__content">
+                    <div className="projects__img projects__img--2"></div>
+                    <div className="project">
+                        <div className="project__option">
+                            <a href="#" className="project__link">watch</a>
+                        </div>
+                        <div className="project__option">
+                            <a href="#" className="project__link">source code</a>
+                        </div>
                     </div>
                 </div>
             </div>
 
             <div className="projects__item projects__item--3">
-                <div className="projects__img projects__img--3"></div>
-                <div className="project">
-                    <div className="project__option">
-                        <a href="#" className="project__link">watch</a>
-                    </div>
-                    <div className="project__option">
-                        <a href="#" className="project__link">source code</a>
+                <div className="projects__content">
+                    <div className="projects__img projects__img--3"></div>
+                    <div className="project">
+                        <div className="project__option">
+                            <a href="#" className="project__link">watch</a>
+                        </div>
+                        <div className="project__option">
+                            <a href="#" className="project__link">source code</a>
+                        </div>
                     </div>
                 </div>
             </div>
