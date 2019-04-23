f2d90e8e393fa9a468036656b2ba0e87f2787583
Rostyslav Yatsura <floxaz1@gmail.com>
2019-04-23 18:34:25 +0200

    setup production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,59 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
-    mode: 'development',
-    entry: './src/app.js',
-    output: {
-        path: path.join(__dirname, 'dist'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
+module.exports = env => {
+    const isProduction = env === 'production';
+    const CSSExtract = new ExtractTextPlugin('styles.css');
+
+    return {
+        mode: 'development',
+        entry: './src/app.js',
+        output: {
+            path: path.join(__dirname, 'dist'),
+            filename: 'bundle.js'
         },
-        {
-            use: ['style-loader', 'css-loader', 'sass-loader'],
-            test: /\.s?css$/
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            },
+            {
+                test: /\.s?css$/,
+                use: CSSExtract.extract({
+                    use: [
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        }
+                    ]
+                })
+            },
+            {
+                loader: 'file-loader',
+                test: /\.(png|jpg)$/,
+                include: path.join(__dirname, 'src'),
+                options: {
+                    outputPath: 'images'
+                }
+            }
+            ]
         },
-        {
-            loader: 'file-loader',
-            test: /\.(png|jpg)$/,
-            include: path.join(__dirname, 'src')
+        plugins: [ CSSExtract ],
+        devtool: isProduction ? 'source-map' : 'cheap-moduel-eval-sourc-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'dist'),
+            open: true,
+            openPage: '',
+            historyApiFallback: true
         }
-    ]
-    },
-    devtool: 'cheap-moduel-eval-sourc-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-        open: true,
-        openPage: '',
-        historyApiFallback: true
     }
-};
+}
