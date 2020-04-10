457130d62bdcc1498aa49c3984ad1229a95402b4
Rostyslav Yatsura <floxaz1@gmail.com>
2020-04-10 15:06:43 +0200

    added a new project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,36 +1,41 @@
-import React from 'react';
-import Project from './Project';
+import React from "react";
+import Project from "./Project";
 
 const Projects = () => (
-    <section className="projects">
-        <div className="row projects__display-flex">
-            <Project
-                number={5}
-                github={'https://github.com/floxaz/luxuryVoyage'}
-                live={'https://luxuryvoyage.herokuapp.com/'}
-            />
-            <Project
-                number={4}
-                github={'https://github.com/floxaz/movie-gallery'}
-                live={'http://moviegallery.rossyatsura.com'}
-            />
-            <Project
-                number={3}
-                github={'https://github.com/floxaz/calculator'}
-                live={'http://calculator.rossyatsura.com'}
-            />
-            <Project
-                number={2}
-                github={'https://github.com/floxaz/weatherApp'}
-                live={'https://floxaz.github.io/weather-app-live'}
-            />
-            <Project
-                number={1}
-                github={'https://github.com/floxaz/top-poems'}
-                live={'https://floxaz.github.io/top-poems'}
-            />
-        </div>
-    </section>
+  <section className="projects">
+    <div className="row projects__display-flex">
+      <Project
+        number={6}
+        github={"https://github.com/floxaz/notepad"}
+        live={"https://notepad-1.herokuapp.com/"}
+      />
+      <Project
+        number={5}
+        github={"https://github.com/floxaz/luxuryVoyage"}
+        live={"https://luxuryvoyage.herokuapp.com/"}
+      />
+      <Project
+        number={4}
+        github={"https://github.com/floxaz/movie-gallery"}
+        live={"http://moviegallery.rossyatsura.com"}
+      />
+      <Project
+        number={3}
+        github={"https://github.com/floxaz/calculator"}
+        live={"http://calculator.rossyatsura.com"}
+      />
+      <Project
+        number={2}
+        github={"https://github.com/floxaz/weatherApp"}
+        live={"https://floxaz.github.io/weather-app-live"}
+      />
+      <Project
+        number={1}
+        github={"https://github.com/floxaz/top-poems"}
+        live={"https://floxaz.github.io/top-poems"}
+      />
+    </div>
+  </section>
 );
 
 export default Projects;
